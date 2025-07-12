f1008bdc8186ada0ba8701b0cedb3dbd1d823775
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-12 22:30:26 +0530

    some progress

---
+++ compiler/cradle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h> //Ill use this directly for isdigit
+
+int main(){
+    const char TAB = '\t';
+    char Look;
+
+    void getChar(){
+        scanf(" %c", &Look); //SKips white space, we need that for tokens
+    }
+
+   
+}
