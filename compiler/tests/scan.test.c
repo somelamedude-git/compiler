79b4969063072dd2d2b6c8bf8c34212984fc8706
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-13 20:00:05 +0530

    toy scanner added

--- compiler/tests/scan.test.c
+++ compiler/tests/scan.test.c
@@ -1,3 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
 char Look;
+
+void getChar(){
+    Look = getchar();
+}
+
+
+void appendChar(char* s, char c){
+    int length = strlen(s);
+    s[length] = c;
+    s[length+1] = '\0';
+}
+
+int main(){
+    getChar();
+    char str[100]="";
+
+    while(Look!=EOF && Look!='\n'){
+        appendChar(str, Look);
+        getChar();
+    }
+
+    getChar();
+
+    char test_str[100]="";
+    
+    while(Look!=EOF && Look !='\n'){
+        appendChar(test_str, Look);
+        getChar();
+    }
+
+    if(strcmp(test_str, str)){
+        printf("%s not specified", test_str);
+    }
+}
